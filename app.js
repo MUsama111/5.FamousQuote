29884d44065fe2bd7c1643a8c5b09151201ae2c7
Muhammad Usama <162746442+MUsama111@users.noreply.github.com>
2024-03-18 21:36:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var Quote = "A person who never made a mistake never tried anything new.";
+var Famous_person = "Albert Einstein";
+var message = "".concat(Famous_person, " once said, ").concat(Quote);
+console.log(message);
+var Quote1 = "I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.";
+var Famous_person1 = "Bruce Lee";
+var Quote2 = "workhard untill your account number looks like a cell phone number";
+var Famous_person2 = "Muhammad Usama";
+var message1 = "".concat(Famous_person1, " ").concat(Quote1);
+var message2 = "".concat(Famous_person2, " said that, ").concat(Quote2);
+console.log(message1);
+console.log(message2);
---
+++ app.ts
@@ -0,0 +1,12 @@
+let Quote: String= "A person who never made a mistake never tried anything new.";
+let Famous_person: String= "Albert Einstein";
+let message= `${Famous_person} once said, ${Quote}`;
+console.log(message);
+let Quote1: String= "I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.";
+let Famous_person1: string= "Bruce Lee";
+let Quote2: string= "workhard untill your account number looks like a cell phone number";
+let Famous_person2: string="Muhammad Usama";
+let message1= `${Famous_person1} ${Quote1}`;
+let message2= `${Famous_person2} said that, ${Quote2}`;
+console.log(message1);
+console.log(message2);
